59b94a4f03ace518d0a9464b7b48020ebf84a1e5
ukarpenkov <truloveu@gmail.com>
2023-08-17 18:35:34 +0300

    init app

--- random-cafe/src/App.vue
+++ random-cafe/src/App.vue
@@ -1,30 +1,14 @@
+<script>
+// import axios from "axios";
+
+// // let data = axios.get(`https://bandaumnikov.ru/api/test/site/get-index`);
+console.log("data");
+let a = 5;
+console.log(a + 3);
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>Тест</div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
