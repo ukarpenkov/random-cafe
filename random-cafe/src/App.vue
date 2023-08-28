3e0c13433de1c3cd273003b7e6d0fba5eca731ee
ukarpenkov <truloveu@gmail.com>
2023-08-28 16:02:49 +0300

    show cards

--- random-cafe/src/App.vue
+++ random-cafe/src/App.vue
@@ -1,42 +1,59 @@
-<script setup>
+<script>
 import axios from "axios";
 import { reactive } from "vue";
-const state = reactive({
-  checked1: true,
-  checked2: false,
-  checked3: false,
-});
-// export default {
-//   data() {
-//     return {
-//       restaurants: [],
-//     };
-//   },
-//   methods: {
-//     async fetchRestaurant() {
-//       try {
-//         const response = await axios.get(`https://bandaumnikov.ru/api/test/site/get-index`);
-//         this.restaurants = response.data.data;
-//       } catch (e) {
-//         console.log(e);
-//       }
-//     },
-//   },
-//   mounted() {
-//     this.fetchRestaurant();
-//   },
-// };
+
+export default {
+  data() {
+    return {
+      restaurants: [],
+    };
+  },
+  methods: {
+    async fetchRestaurant() {
+      try {
+        const response = await axios.get(`https://bandaumnikov.ru/api/test/site/get-index`);
+        this.restaurants = response.data.data;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+  mounted() {
+    this.fetchRestaurant();
+  },
+};
 </script>
 
 <template>
-  <a-space direction="vertical">
-    <a-switch v-model:checked="state.checked1" checked-children="开" un-checked-children="关" />
-    <a-switch v-model:checked="state.checked2" checked-children="1" un-checked-children="0" />
-    <a-switch v-model:checked="state.checked3">
-      <template #checkedChildren><check-outlined /></template>
-      <template #unCheckedChildren><close-outlined /></template>
-    </a-switch>
-  </a-space>
+  <a-divider orientation="left">Vertical</a-divider>
+  <a-row :gutter="[16, 24]">
+    <a-col class="gutter-row" v-for="restaurant in restaurants" :span="6">
+      <div class="gutter-box">
+        <a-card hoverable>
+          <template #cover>
+            <img v-if="restaurant.photo !== ''" alt="example" :src="restaurant.photo" />
+            <img
+              v-else="restaurant.photo === ''"
+              alt="example"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />
+          </template>
+          <a-card-meta :title="restaurant.name">
+            <template #description>
+              <div>{{ restaurant.name }}</div>
+              <div>{{ restaurant.cuisine }}</div>
+              <div>{{ restaurant.distance }}</div>
+            </template>
+          </a-card-meta>
+        </a-card>
+      </div>
+    </a-col>
+  </a-row>
 </template>
 
-<style></style>
+<style scoped>
+.gutter-box {
+  padding: 8px 0;
+  margin: 5px;
+}
+</style>
