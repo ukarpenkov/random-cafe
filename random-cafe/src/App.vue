9752306d49fd771e7adea7bfbe107d263ff6e5e6
ukarpenkov <truloveu@gmail.com>
2023-08-30 16:01:10 +0300

    cards description

--- random-cafe/src/App.vue
+++ random-cafe/src/App.vue
@@ -31,18 +31,40 @@ export default {
       <div class="gutter-box">
         <a-card hoverable>
           <template #cover>
-            <img v-if="restaurant.photo !== ''" alt="example" :src="restaurant.photo" />
+            <img
+              v-if="restaurant.photo !== ''"
+              alt="example"
+              :src="restaurant.photo"
+              :height="300"
+            />
             <img
               v-else="restaurant.photo === ''"
               alt="example"
               src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              :height="300"
             />
           </template>
           <a-card-meta :title="restaurant.name">
             <template #description>
-              <div>{{ restaurant.name }}</div>
-              <div>{{ restaurant.cuisine }}</div>
-              <div>{{ restaurant.distance }}</div>
+              <div v-if="restaurant.cuisine !== ''">
+                <span class="card-attribute">Кухня:</span> {{ restaurant.cuisine }}
+              </div>
+              <div v-if="restaurant.address !== ''">
+                <span class="card-attribute">Адрес:</span> {{ restaurant.address }}
+              </div>
+              <div v-if="restaurant.landmark !== ''">
+                <span class="card-attribute">Ориентир:</span> {{ restaurant.landmark }}
+              </div>
+              <div v-if="restaurant.distance > 0">
+                <span class="card-attribute">Расстояние:</span> {{ restaurant.distance }} метров
+              </div>
+              <div v-if="restaurant.time > 0">
+                <span class="card-attribute">Время до места:</span> {{ restaurant.time }} минут
+              </div>
+              <a-tag color="purple" v-if="restaurant.business_lunch === true">
+                Бизнес ланч:
+                {{ restaurant.price }}₽
+              </a-tag>
             </template>
           </a-card-meta>
         </a-card>
@@ -55,5 +77,9 @@ export default {
 .gutter-box {
   padding: 8px 0;
   margin: 5px;
+}
+
+.card-attribute {
+  font-weight: bold;
 }
 </style>
