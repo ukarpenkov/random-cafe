bac70895e0c1fcaef37f21a5264e379613873e91
ukarpenkov <truloveu@gmail.com>
2023-09-19 19:10:21 +0300

    roulette logic

--- random-cafe/src/components/RandomButton.vue
+++ random-cafe/src/components/RandomButton.vue
@@ -24,14 +24,16 @@ export default {
       }
     },
     start() {
-      console.log(this.$refs.cardsBlock);
-      var random = Math.floor(Math.random() * 22); // От 0 до 8
-      this.$refs.cardsBlock.style.left = -random * 100 + "px";
-      // setTimeout(function () {
-      //   random++;
-      //   this.$refs.cards[random].style.background = "#7B90F7";
-      //   this.$refs.cards[random].style.color = "white";
-      // }, 5000);
+      let random = Math.floor(Math.random() * 20); // От 0 до 8
+      this.$refs.cards.style.left = -random * 200 + "px";
+
+      console.log(this.$refs.cards);
+      console.log(this.$refs.card[random]);
+      setTimeout(() => {
+        random++;
+        this.$refs.card[random].style.background = "#7B90F7";
+        this.$refs.card[random].style.color = "white";
+      }, 3000);
     },
   },
   mounted() {
@@ -55,47 +57,24 @@ export default {
     >
       <div class="shadow"></div>
       <div class="random">
-        <div ref="cardsBlock">
-          <div class="cards" v-for="restaurant in restaurants">
-            <a-card hoverable style="width: 240px">
-              <template #cover>
-                <img
-                  v-if="restaurant.photo !== ''"
-                  alt="example"
-                  :src="restaurant.photo"
-                  :height="300"
-                />
-                <img
-                  v-else="restaurant.photo === ''"
-                  alt="example"
-                  src="./../images/default-cafe.jpg"
-                  :height="300"
-                />
-              </template>
-              <a-card-meta :title="restaurant.name">
-                <template #description>
-                  <div v-if="restaurant.cuisine !== ''">
-                    <span class="card-attribute">Кухня:</span> {{ restaurant.cuisine }}
-                  </div>
-                  <div v-if="restaurant.address !== ''">
-                    <span class="card-attribute">Адрес:</span> {{ restaurant.address }}
-                  </div>
-                  <div v-if="restaurant.landmark !== ''">
-                    <span class="card-attribute">Ориентир:</span> {{ restaurant.landmark }}
-                  </div>
-                  <div v-if="restaurant.distance > 0">
-                    <span class="card-attribute">Расстояние:</span> {{ restaurant.distance }} метров
-                  </div>
-                  <div v-if="restaurant.time > 0">
-                    <span class="card-attribute">Время до места:</span> {{ restaurant.time }} минут
-                  </div>
-                  <a-tag color="purple" v-if="restaurant.business_lunch === true">
-                    Бизнес ланч:
-                    {{ restaurant.price }}₽
-                  </a-tag>
-                </template>
-              </a-card-meta>
-            </a-card>
+        <div ref="cards" class="cards">
+          <div ref="card" class="card" v-for="restaurant in restaurants">
+            <div>
+              <img
+                v-if="restaurant.photo !== ''"
+                alt="example"
+                :src="restaurant.photo"
+                class="card-image"
+              />
+              <img
+                v-else="restaurant.photo === ''"
+                alt="example"
+                src="./../images/default-cafe.jpg"
+                class="card-image"
+              />
+            </div>
+
+            <div>{{ restaurant.name }}</div>
           </div>
         </div>
       </div>
@@ -134,7 +113,7 @@ export default {
 
 .cards {
   height: 100%;
-  transition: 5s ease;
+  transition: 2s ease;
 }
 
 :where(.css-dev-only-do-not-override-1ugq3w3).ant-modal .ant-modal-body {
@@ -150,8 +129,8 @@ export default {
   transform: translate(-50%, -50%);
   border-radius: 10px;
   border: 2px solid white;
-  width: 400px;
-  height: 100px;
+  width: 800px;
+  height: 250px;
   box-shadow: inset 0px 0px 20px 0px black;
 }
 
@@ -162,8 +141,8 @@ export default {
   top: 50%;
   transform: translate(-50%, -50%);
   border-radius: 10px;
-  width: 400px;
-  height: 100px;
+  width: 800px;
+  height: 250px;
   background: #161d31;
 }
 
@@ -171,14 +150,18 @@ export default {
   position: absolute;
   left: 0;
   top: 0;
-  width: 2000px;
+  width: 1000%;
   height: 100%;
   transition: 5s ease;
 }
 
-.cards {
+.card-image {
+  height: 150px;
+  width: 200px;
+}
+
+.card {
   position: relative;
-  display: flex;
   justify-content: center;
   align-items: center;
   float: left;
@@ -187,8 +170,8 @@ export default {
   font-size: 20px;
   margin-top: 5px;
   margin-left: 5px;
-  width: 100px;
-  height: 90px;
+  width: 200px;
+  height: 250px;
   color: #6a6a6a;
   background: white;
   border-radius: 10px;
